fe2921d2ada062b37dd1c33c7bb6c2bdeb0b94dc
V3dantt <158360445+VedantDalvi05@users.noreply.github.com>
2025-09-02 15:00:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
-const opt= document.getElementById("otp");
-const num1= document.getElementById("num1");
-const num2= document.getElementById("num2");
-const num3= document.getElementById("num3");
-const num4= document.getElementById("num4");
-const min=0;
-const max= 9;
-let randomNum1;
-let randomNum2;
-let randomNum3;
-let randomNum4;
+const min=1;
+const max=100;
 
-otp.onclick = function (){
-    randomNum1 = Math.floor(Math.random()*max)+min;
-    randomNum2 = Math.floor(Math.random()*max)+min;
-    randomNum3 = Math.floor(Math.random()*max)+min;
-    randomNum4 = Math.floor(Math.random()*max)+min;
-    num1.textContent = randomNum1;
-    num2.textContent = randomNum2;
-    num3.textContent = randomNum3;
-    num4.textContent = randomNum4;
+const ans= Math.floor(Math.random() * (max-min+1))+min;
+
+let attempts=0;
+let guess;
+let running =true;
+
+
+
+while(running){
+guess=Number(window.prompt(`Guess any number between ${min} and ${max}`));
+
+if(isNaN(guess)){
+    window.alert("Please enter a valid number");
 
 }
+else if(guess<min){
+    window.alert(`Please enter a number greater than ${min}`);
+}
+
+else if(guess>max){
+    window.alert(`Please enter a number smaller than ${max}`);
+    
+}
+else{
+    attempts++;
+    if(ans<guess){
+    window.alert(`Too High, try againn!`);
+        
+    }
+    else if(ans>guess){
+    window.alert(`Too Low, try again !`);
+        
+    }
+    else{
+    window.alert(`Congratulations !!, The correct answer is ${ans} , It took you ${attempts} attempts to guess it correctly`);
+
+    running=false;
+        
+    }
+    }
+}
+
+
+
+
+
+
+
+/* '------------------------------------._
+'------------------------------------._
+   .-------------------------------------'
+    `-. _          _.-'
+ _   /     `'----------------'      _
+( )  |     C O D E   B Y   V 3 D N T T     | ( )
+ `--./         _.-'        `--'
+  .-'-------------------------------------`--'
+`------------------------------------._
+`------------------------------------._
+*/
+
+
